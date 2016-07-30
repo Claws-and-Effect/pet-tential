fa2afb80d1eb1445cb062a59fa7d58c17a8440e8
Ubuntu <Claws@Pet-tential.v2ub0irjl3nuxfefl5d5jtbqih.px.internal.cloudapp.net>
2016-07-30 03:37:50 +0000

    moved things into separate php files

--- header.php
+++ header.php
@@ -50,40 +50,4 @@
                 </ul>
               </nav>
             </div>
-          </div>
-
-          <div class="inner cover">
-            <h1 class="cover-heading">Welcome to Pet-tential!</h1>
-            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
-            <div class="row">
-              <img src="/paws.svg" class="img-responsive center-block">
-            </div>
-            <p class="lead">
-              <a href="#" class="btn btn-lg btn-default">Learn more</a>
-            </p>
-          </div>
-
-          <div class="mastfoot">
-            <div class="inner">
-              <p>GovHack 2016 Project by <i>Claws and Effect</i></p>
-              <p>Cover template for <a href="http://getbootstrap.com">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a></p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
-
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require 'header.php'
+?>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Welcome to Pet-tential!</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <div class="row">
+              <img src="/paws.svg" class="img-responsive center-block">
+            </div>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+            </p>
+</div>
+<?php
+require 'footer.php'
+?>
